c5c51562f109a3823b1bb44fe1b1885791dde38c
Venkatesh Manchala <manchalavenkatesh10@gmail.com>
2023-09-29 19:22:12 +0530

    changes in resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './about.css';
 import AboutImg from "../../assets/profile.jpeg";
-import CV from "../../assets/RESUMEV.pdf";
+import CV from "../../assets/VenkateshResume.pdf";
 import Info from './Info';
 const About = () => {
     return(
